72744eabe5092d3abbcb162cbbb9d484ce6a9d62
Maninder Kaur <maninder702@gmail.com>
2025-05-26 13:11:09 +0530

    Delete odd_even_number.py

--- odd_even_number.py
+++
@@ -1,8 +0,0 @@
-#Program to check if given number is odd or even
-
-num = int(input("Enter any number:"))
-
-if(num % 2 == 0):
-    print("Entered number is even")
-else:
-    print("Entered number is odd")
